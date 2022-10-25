035575ab4e46e0d0bfebf65ea3cfe08ff840972a
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-10-25 16:47:02 +0100

    working on Menu Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { useState } from "react";
+import Menu from "./Menu";
+import Category from "./Categories";
+import Items from "./Data";
+
 function App() {
-  return <div>Hello world</div>;
+  const [menuItem, setMenuItem] = useState(Items);
+  const [categories, setCategories] = useState([]);
+
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>Our Menu</h2>
+          <div className="underline"></div>
+        </div>
+
+        <Category />
+        <Menu />
+      </section>
+    </main>
+  );
 }
 
 export default App;
