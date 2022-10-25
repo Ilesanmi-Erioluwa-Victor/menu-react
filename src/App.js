f6e1fbd0b038a1a0e169ffd2f4cedb897b3e1959
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-10-25 17:29:27 +0100

    Experiencing a weird bug on my Image

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import Menu from "./Menu";
-import Category from "./Categories";
-import Items from "./Data";
+import Categories from "./Categories";
+import items from "./Data";
+const allCategories = ["all", ...new Set(items.map((item) => item.category))];
 
 function App() {
-  const [menuItem, setMenuItem] = useState(Items);
-  const [categories, setCategories] = useState([]);
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === "all") {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
 
   return (
     <main>
       <section className="menu section">
         <div className="title">
-          <h2>Our Menu</h2>
+          <h2>our menu</h2>
           <div className="underline"></div>
         </div>
-
-        <Category />
-        <Menu />
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
       </section>
     </main>
   );
